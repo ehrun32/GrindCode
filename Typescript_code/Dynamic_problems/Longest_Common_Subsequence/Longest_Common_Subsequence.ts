df2c16dd7995226baed6bd13d672e18472606b69
Arun Cheriakara <57046416+ehrun32@users.noreply.github.com>
2024-10-19 09:39:35 -0400

    Longest_Common_Subsequence

---
+++ Typescript_code/Dynamic_problems/Longest_Common_Subsequence/Longest_Common_Subsequence.ts
@@ -0,0 +1,27 @@
+// Longest_Common_Subsequence
+const text1 = "abcde";
+const text2 = "ace";
+// output is 3
+
+function LCS(text1: string, text2: string): number {
+  const m = text1.length;
+  const n = text2.length;
+
+  const dp: number[][] = Array.from({ length: m + 1 }, () =>
+    Array(n + 1).fill(0)
+  );
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = 1 + dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+  console.log(dp);
+  return dp[m][n];
+}
+
+console.log(LCS(text1, text2));
