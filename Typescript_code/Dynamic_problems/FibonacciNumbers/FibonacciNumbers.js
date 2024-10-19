dfa50a363fdf828517a73c55ec1f04a8da8374a4
Arun Cheriakara <57046416+ehrun32@users.noreply.github.com>
2024-10-19 10:24:49 -0400

    FibonacciNumbers

---
+++ Typescript_code/Dynamic_problems/FibonacciNumbers/FibonacciNumbers.js
@@ -0,0 +1,10 @@
+function fib(n) {
+    const dp = new Array(n).fill(0);
+    dp[0] = 0;
+    dp[1] = 1;
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+}
+console.log(fib(10));
