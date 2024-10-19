5b3ab8241b2669cb662cdd62d705612329b7b315
Arun Cheriakara <57046416+ehrun32@users.noreply.github.com>
2024-10-19 08:57:15 -0400

    Longest_Increasing_Subsequence

---
+++ Typescript_code/Longest_Increasing_Subsequence/Longest_Increasing_Subsequence.ts
@@ -0,0 +1,16 @@
+const A = [3, 4, -1, 0, 6, 2, 3];
+
+function lengthOfLIS(nums: number[]): number {
+  const dp = new Array(nums.length).fill(1);
+
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] < nums[i]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+  return Math.max(...dp);
+}
+
+console.log(lengthOfLIS(A));
